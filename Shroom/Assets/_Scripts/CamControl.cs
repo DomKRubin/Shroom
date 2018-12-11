343286fcfcd7c0a4326c4c0df4f48f7a0937154f
DomKRubin <43648534+DomKRubin@users.noreply.github.com>
2018-12-11 11:32:45 -0500

    Initial commit

---
+++ Shroom/Assets/_Scripts/CamControl.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamControl : MonoBehaviour {
+    Vector2 mouseLook, smoothV;
+    public float sensitivity, smoothing;
+
+    private GameObject character;
+	
+	void Start () {
+        character = this.transform.parent.gameObject;
+	}
+
+	void Update () {
+        //Recieve mouse direction
+        var md = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+        //Apply smoothing and sensitivity
+        md = Vector2.Scale(md, new Vector2((sensitivity * smoothing), (sensitivity * smoothing)));
+        smoothV.x = Mathf.Lerp(smoothV.x, md.x, 1f / smoothing);
+        smoothV.y = Mathf.Lerp(smoothV.y, md.y, 1f / smoothing);
+        mouseLook += smoothV;
+
+        mouseLook.y = Mathf.Clamp(mouseLook.y, -85, 85);
+
+        //print(mouseLook.y);
+
+        if (Input.GetKeyDown("escape"))
+        {
+            sensitivity = 0;
+            smoothing = 0;
+        }
+
+        //Use the new attributes to move the Camera
+        transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+        character.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, character.transform.up);
+
+    }
+}
---
+++ Shroom/Assets/_Scripts/Movement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+    public float moveSpeed;
+	
+	void Start () {
+        Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+	
+	void Update () {
+        float x = Input.GetAxis("Vertical") * moveSpeed;
+        float z = Input.GetAxis("Horizontal") * moveSpeed;
+
+        x *= Time.deltaTime;
+        z *= Time.deltaTime;
+
+        transform.Translate(z, 0, x);
+
+        if (Input.GetKeyDown("escape"))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+
+        if (Input.GetKeyDown("left shift"))
+        {
+            moveSpeed *= 2;
+        }
+
+        if (Input.GetKeyUp("left shift"))
+        {
+            moveSpeed /= 2;
+        }
+
+    }
+}
